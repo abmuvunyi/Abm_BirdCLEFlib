952cf538dd1da7b992073593b71f1b7003a9f4f2
abmuvunyi <ab.muvunyi@gmail.com>
2025-05-01 22:11:48 +0200

    Export BirdCLEFDataset in data package

--- data/__init__.py
+++ data/__init__.py
@@ -2,8 +2,12 @@ from .tokenizer import H4Tokenizer
 from .lm_dataset import LMDataset
 from .asr_dataset import ASRDataset
 from .verify_dataloader import verify_dataloader
+from .birdclef_dataset import BirdCLEFDataset   # ← new line
 
-__all__ = ['H4Tokenizer', 
-           'LMDataset', 
-           'ASRDataset',
-           'verify_dataloader']
+__all__ = [
+    'H4Tokenizer',
+    'LMDataset',
+    'ASRDataset',
+    'verify_dataloader',
+    'BirdCLEFDataset'        # ← added here
+]
